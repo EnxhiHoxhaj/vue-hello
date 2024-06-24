4acebc797d3356249143ed20f03483490c10a76c
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-24 15:44:20 +0200

    Feat: add img

--- Bonus/main.js
+++ Bonus/main.js
@@ -11,6 +11,8 @@ createApp ({
             salutiVue: "HELLO VUE WORLD!!!",
             myclass: "withe", 
             fCont: "flex-container",
+            imgClass: "img_class ",
+            imgSaluto: "img_saluto",
         }
     }
 }).mount(`#app`)
